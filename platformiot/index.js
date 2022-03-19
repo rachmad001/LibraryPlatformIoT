e0fea341abc6da152f24d5e5cbcb5a6084696350
rachmad001 <rachmadsukri@gmail.com>
2022-03-19 22:54:34 +0700

    device and data function

--- platformiot/index.js
+++ platformiot/index.js
@@ -8,6 +8,12 @@ module.exports = class Platform {
         this.getListProjectResponse = "";
         this.editProjectResponse = "";
         this.deleteProjectResponse = "";
+        this.createDeviceResponse = "";
+        this.getListDeviceResponse = "";
+        this.editDeviceResponse = "";
+        this.deleteDeviceResponse = "";
+        this.addDataResponse = "";
+        this.getDataResponse = "";
     }
     createProject(nameProject, description) {
         this.createProjectResponse = "";
@@ -106,6 +112,152 @@ module.exports = class Platform {
         })
         req.on('error', error => {
             this.deleteProjectResponse = 'error : '+error.message
+        })
+        req.end()
+    }
+    createDevice(nameProject, nameDevices){
+        this.createDeviceResponse = "";
+        const data = JSON.stringify({
+            token: this.token,
+            project: nameProject,
+            devices: nameDevices
+        })
+        const options = {
+            hostname: this.server,
+            port: this.port,
+            path: '/create/devices',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const req = this.http.request(options, res => {
+            res.on('data', d => {
+                this.createDeviceResponse = d.toString();
+            })
+        })
+        req.write(data);
+        req.on('error', error => {
+            this.createDeviceResponse = "error : "+error.message;
+        })
+        req.end();
+    }
+    getListDevice(nameProject){
+        this.getListDeviceResponse = "";
+        const options = {
+            hostname: this.server,
+            port: this.port,
+            path: '/listDevices/'+nameProject,
+            method: 'GET',
+            headers: {
+                'token': this.token
+            }
+        }
+        const req = this.http.request(options, res => {
+            res.on('data', d => {
+                this.getListDeviceResponse = d.toString();
+            })
+        })
+        req.on('error', error => {
+            this.getListDeviceResponse = 'error : '+error.message;
+        })
+        req.end()
+    }
+    editDevice(nameProject, oldNameDevice, newNameDevie){
+        this.editDeviceResponse = "";
+        const data = JSON.stringify({
+            project: nameProject,
+            oldname: oldNameDevice,
+            newname: newNameDevie
+        })
+        const options = {
+            hostname: this.server,
+            port: this.port,
+            path: '/edit/devices',
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': this.token
+            }
+        }
+        const req = this.http.request(options, res => {
+            res.on('data', d => {
+                this.editDeviceResponse = d.toString();
+            })
+        })
+        req.write(data);
+        req.on('error', error => {
+            this.editDeviceResponse = 'error : '+error.message;
+        })
+        req.end()
+    }
+    deleteDevice(nameProject, nameDevice){
+        this.deleteDeviceResponse = "";
+        const options = {
+            hostname: this.server,
+            port: this.port,
+            path: '/delete/'+nameProject+'/'+nameDevice,
+            method: 'DELETE',
+            headers: {
+                'token': this.token
+            }
+        }
+        const req = this.http.request(options, res => {
+            res.on('data', d => {
+                this.deleteDeviceResponse = d.toString();
+            })
+        })
+        req.on('error', error => {
+            this.deleteDeviceResponse = 'error : '+error.message;
+        })
+        req.end()
+    }
+    addData(nameProject, nameDevice, dataDevices){
+        this.addDataResponse = "";
+        const data = JSON.stringify({
+            token: this.token,
+            nameproject: nameProject,
+            data: dataDevices,
+            namedevices: nameDevice
+        })
+        const options = {
+            hostname: this.server,
+            port: this.port,
+            path: '/addData',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const req = this.http.request(options, res => {
+            res.on('data', d => {
+                this.addDataResponse = d.toString();
+            })
+        })
+        req.write(data);
+        req.on('error', error => {
+            this.addDataResponse = 'error : '+error.message;
+        })
+        req.end()
+    }
+    getData(nameProject, nameDevice){
+        this.getDataResponse = "";
+        const options = {
+            hostname: this.server,
+            port: this.port,
+            path: '/data/'+nameProject+'/'+nameDevice,
+            method: 'GET',
+            headers: {
+                'token': this.token
+            }
+        }
+        const req = this.http.request(options, res => {
+            res.on('data', d => {
+                this.getDataResponse = d.toString();
+            })
+        })
+        req.on('error', error => {
+            this.getDataResponse = 'error : '+error.message;
         })
         req.end()
     }
