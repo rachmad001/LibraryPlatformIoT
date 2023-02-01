c4ceb3da5184291b8ccdcc0b4a37fa0bfe538f51
rachmad001 <rachmadsukri@gmail.com>
2023-02-01 21:23:13 +0700

    add get data

---
+++ Platform/examples/PlatformAddData/PlatformAddData.ino
@@ -0,0 +1,33 @@
+#include "Platform.h"
+#include <ArduinoJson.h>
+
+String token =  "ptiZNNdIPfgjMqtQziTXN6cbF";
+String server = "http://192.168.137.1:8000";
+
+String ssid = "OPPO A12";
+String password = "1234duakali";
+
+unsigned long lastTime = 0;
+unsigned long interval = 1000;
+
+String projectName = "rumahan";
+String devices = "testing";
+
+Platform platform(token, server);
+
+void setup() {
+  Serial.begin(9600);
+  platform.connectWiFi(ssid, password);
+}
+
+void loop() {
+  StaticJsonDocument<200> doc;
+  if((millis() - lastTime) > interval){
+    doc["namadepan"] = "rachmad";
+    doc["namabelakang"] = "sukri";
+    String data = "";
+    serializeJson(doc, data);
+    platform.sendData(projectName, devices, data);
+    lastTime = millis();
+  }
+}
---
+++ Platform/examples/PlatformGetData/PlatformGetData.ino
@@ -0,0 +1,30 @@
+#include "Platform.h"
+#include <ArduinoJson.h>
+
+String token =  "ptiZNNdIPfgjMqtQziTXN6cbF";
+String server = "http://192.168.137.1:8000";
+
+String ssid = "OPPO A12";
+String password = "1234duakali";
+
+unsigned long lastTime = 0;
+unsigned long interval = 1000;
+
+String projectName = "rumahan";
+String devices = "testing";
+
+Platform platform(token, server);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  platform.connectWiFi(ssid, password);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if((millis() - lastTime) > interval){
+    platform.get(projectName, devices);
+    String data = Platform.getData();
+  }
+}
